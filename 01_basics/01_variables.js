e2a3494e28ce1b58e7aaca9d8613aab846764de0
Amit Kumar Patro <141933834+patro-amit@users.noreply.github.com>
2025-02-03 18:28:08 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accId = 12324
+let accName = "hello"
+var accPass = "090909"
+accCity = "Bam"
+let accState = null
+
+// accId = 56544
+
+accName = "bye"
+accPass = "424242"
+accCity = "Chennai"
+
+console.log(accId)
+console.table([accId, accName, accPass, accCity, accState])

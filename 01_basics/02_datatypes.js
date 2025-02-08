e850edd3ee01a32b0b88268fb0bff453654f2224
Amit Kumar Patro <141933834+patro-amit@users.noreply.github.com>
2025-02-08 17:18:24 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we can't use it in console cause it's not an browser
+
+console.log(3+3); console.log("hello") // not a good practice
+
+let name = 'amit'
+let age = 19
+let isLoggedin = false
+
+// number = 2 to power 53
+// bigint use is not recomended for beginners
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined
+// symbol = unique
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);
+

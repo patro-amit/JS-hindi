1c4463169d212af1a3478bbb9325512b79b9a5c8
Amit Kumar Patro <141933834+patro-amit@users.noreply.github.com>
2025-02-15 12:13:11 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = false
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let value = Number(score)
+
+// let value = String(score)
+
+// console.log(typeof value);
+// console.log(value)
+
+let isLoggedIn = "0"
+
+let booleanCheck = Boolean(isLoggedIn)
+console.log(booleanCheck);

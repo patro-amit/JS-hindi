e05a248b4a8a245c8c0c93f15881ed358545c8ce
Amit Kumar Patro <141933834+patro-amit@users.noreply.github.com>
2025-02-16 09:31:14 +0000

    Comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(1 > "2");
+// console.log(1 > "02");
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null < 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+
+console.log(6 >>> 2);
+console.log("02" >>> "2");
+
+

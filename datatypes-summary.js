bac2149b65e4a34dbde88dd27889acf4bb7c157b
Amit Kumar Patro <141933834+patro-amit@users.noreply.github.com>
2025-02-16 16:13:42 +0000

    datatypes-summary

---
+++ datatypes-summary.js
@@ -0,0 +1,31 @@
+const score = 100
+const scoreValue = 100.2
+
+const isLoggedIn = false
+const outsideTemp = null
+let emailId; // emailId = undefined
+
+// console.log(emailId)
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id == anotherId);
+
+const bigInt = 457697845354353457577328n
+
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "hitesh",
+    age: 22
+}
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+
+console.log(typeof id);
+
+// console.log();

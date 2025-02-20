691d7fca0d79746287d7de07dd7d634f1f70eb93
Amit Kumar Patro <141933834+patro-amit@users.noreply.github.com>
2025-02-20 18:56:28 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,33 @@
+const name = "Amit"
+const count = 44
+
+console.log(`Hello my name is ${name} and my repo count is ${count}`); // backticks method of writing
+
+// const game = new String("Hittes-ch")
+
+// console.log(game[0]);
+// console.log(game.__proto__);
+// console.log(game.length);
+// console.log(game.toLocaleUpperCase());
+// console.log(game.charAt(4));
+// console.log(game.indexOf('h'));
+
+// const newString = game.substring(0, 4)
+// // console.log(newString)
+
+// const anotherString = game.slice(-8, 4)
+// // console.log(anotherString)
+
+const newString1 = "        my name is amit. Nice to meet you.   '    \n     iuhughei    "
+// console.log(newString1);
+// console.log(newString1.trim());
+
+const url = "https://hitesh.com/hites%20choudhary"
+
+// console.log(url);
+// console.log(url.replace("%20", "-"));
+// console.log(url.includes('//'));
+
+const gameName = new String("Hites - ch - hello")
+let word = gameName.split(' ')
+console.log(word[2]);

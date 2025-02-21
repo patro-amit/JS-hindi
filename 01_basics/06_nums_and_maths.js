606ce30f26931b5fb38eb456de5c276f792e184f
Amit Kumar Patro <141933834+patro-amit@users.noreply.github.com>
2025-02-21 18:51:15 +0000

    number and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,36 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3).length);
+
+const newNums = 23.7893
+
+// console.log(newNums.toPrecision(2));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString("en-in"));
+
+// +++++++++++++++ MATHS +++++++++++++++++
+
+// console.log(Math); // Object [Math] {}
+// console.log(Math.abs(-4));
+// console.log(Math.round(4*2.4));
+// console.log(Math.ceil(5.4));
+// console.log(Math.floor(8.8));
+// console.log(Math.PI);
+// console.log(Math.sqrt(24));
+// console.log(Math.sqrt(25));
+// console.log(Math.min(3, 6, 7, 2));
+// console.log(Math.max(3, 6, 7, 2));
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10, max = 20
+console.log(Math.floor(Math.random() * (max - min + 1) + min))
